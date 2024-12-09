7b7da9209e2b67fb74aff1ca2d99948bc5ae459a
Dana L <dana.luebke@gmx.de>
2024-12-09 14:02:00 +0100

    Add files via upload

--- Database/keys.py
+++ Database/keys.py
@@ -1,7 +1,6 @@
 #insta_key ='228|7f0ne9zk4gtLY3Iblz8AcOyVFm28zY6pDR6kseSsc00f85cc'
 
 # rapid_API_insta_key = '4cc1dcf696mshc0bce2298261597p109b94jsnb83af5ebff13' # gmx
-# rapid_API_insta_key = 'd0d29cda0bmsh87bcb998e73d3ddp1a3276jsnd764257272bd' # google
-#rapid_API_insta_key = 'd0d29cda0bmsh87bcb998e73d3ddp1a3276jsnd764257272bd'
+rapid_API_insta_key = 'd0d29cda0bmsh87bcb998e73d3ddp1a3276jsnd764257272bd' # google
 
-key_scraper = '4cc1dcf696mshc0bce2298261597p109b94jsnb83af5ebff13'
+
