f6d45f4bfa01c5fb45ea6c351365cb04897e4e1a
Dana L <dana.luebke@gmx.de>
2024-12-09 14:04:10 +0100

    Update keys.py

--- Database/keys.py
+++ Database/keys.py
@@ -1,6 +1,4 @@
-#insta_key ='228|7f0ne9zk4gtLY3Iblz8AcOyVFm28zY6pDR6kseSsc00f85cc'
-
-# rapid_API_insta_key = '4cc1dcf696mshc0bce2298261597p109b94jsnb83af5ebff13' # gmx
-rapid_API_insta_key = 'd0d29cda0bmsh87bcb998e73d3ddp1a3276jsnd764257272bd' # google
+# Rapid API Keys
+rapid_API_insta_key = 'Your Key' 
 
 
